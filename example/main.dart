fb12d5ec683ad2f32740d56bb24a56148bd279ae
Bruce Santier <banzaigm@gmail.com>
2025-02-08 22:08:36 -0800

    Use assets for demo

--- example/main.dart
+++ example/main.dart
@@ -21,36 +21,17 @@ class MyApp extends StatelessWidget {
             child: fling.Gallery(
               preferredRowHeight: 300.0,
               children: <Widget>[
-                Container(
-                  width: 250.0,
-                  height: 700.0,
-                  color: Colors.red,
-                ),
-                Container(
-                  width: 400.0,
-                  height: 600.0,
-                  color: Colors.blue,
-                ),
-                Container(
-                  width: 200.0,
-                  height: 1000.0,
-                  color: Colors.green,
-                ),
-                Container(
-                  width: 150.0,
-                  height: 700.0,
-                  color: Colors.yellow,
-                ),
-                Container(
-                  width: 300.0,
-                  height: 800.0,
-                  color: Colors.purple,
-                ),
-                Container(
-                  width: 100.0,
-                  height: 700.0,
-                  color: Colors.orange,
-                ),
+                Image.asset("assets/images/img_01.jpg"),
+                Image.asset("assets/images/img_02.jpg"),
+                Image.asset("assets/images/img_03.jpg"),
+                Image.asset("assets/images/img_04.jpg"),
+                Image.asset("assets/images/img_05.jpg"),
+                Image.asset("assets/images/img_06.jpg"),
+                Image.asset("assets/images/img_07.jpg"),
+                Image.asset("assets/images/img_08.jpg"),
+                Image.asset("assets/images/img_09.jpg"),
+                Image.asset("assets/images/img_10.jpg"),
+                Image.asset("assets/images/img_11.jpg"),
               ],
             ),
           ),
